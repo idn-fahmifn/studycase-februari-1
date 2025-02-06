37fac31a4e45c9a07ba702e0a87e6f962a4d4d1c
idn-fahmifn <fahmifn.idn@gmail.com>
2025-02-06 16:15:52 +0700

    form tanggapan

--- resources/views/admin/respon/respon.blade.php
+++ resources/views/admin/respon/respon.blade.php
@@ -35,6 +35,10 @@
                                     <th>Dibuat Pada</th>
                                     <td>{{$data->tanggal_laporan->diffForHumans()}}</td>
                                 </tr>
+                                <tr>
+                                    <th>Status</th>
+                                    <td>{{$data->status}}</td>
+                                </tr>
                             </table>
                             {{ $data->deskripsi }}
                         </div>
@@ -44,6 +48,55 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body p-4">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h5 class="text-bold">Judul</h5>
+                    <span class="text-sm">3 jam yang lalu</span>
+                </div>
+                <div class="mt-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolore culpa, autem, quas ipsam esse accusantium molestias unde accusamus, eaque dolor ea excepturi et fugiat non voluptate architecto iure distinctio molestiae! Eveniet eaque minus quaerat exercitationem minima quisquam labore quasi mollitia est delectus, error laudantium cumque nihil, dolore doloremque veniam.
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Tanggapi Laporan</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="#" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group mt-2">
+                        <label class="text-primary">Judul Respon</label>
+                        <input type="text" name="judul_respon" required class="form-control">
+                    </div>
+                    <div class="form-group mt-2">
+                        <label class="text-primary">Dokumentasi (jika ada)</label>
+                        <input type="file" name="dokumentasi" class="form-control">
+                    </div>
+                    <div class="form-group mt-2">
+                        <label class="text-primary">Deskripsi</label>
+                        <textarea name="deskripsi" class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-primary">Tanggapi</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
